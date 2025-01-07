00917ffbe370716e9ecfcc6e7668ae8cae1bd703
nasr7322 <nasr7322@gmail.com>
2025-01-07 20:49:28 +0200

    init

--- api/index.js
+++ api/index.js
@@ -3,14 +3,12 @@ import cors from 'cors';
 import express from 'express';
 
 const app = express();
-const port = 3001;
 
 app.use(cors());
 app.use(express.json());
 
 const LEETCODE_GRAPHQL_ENDPOINT = 'https://leetcode.com/graphql';
 
-// Query to get user profile data
 const profileQuery = `
   query userProfile($username: String!) {
     matchedUser(username: $username) {
@@ -50,7 +48,6 @@ const profileQuery = `
   }
 `;
 
-// Query to get user's recent submissions
 const submissionsQuery = `
   query recentSubmissions($username: String!) {
     recentSubmissionList(username: $username) {
@@ -67,7 +64,6 @@ app.get('/:username', async (req, res) => {
   try {
     const { username } = req.params;
 
-    // Get profile data
     const profileResponse = await axios.post(LEETCODE_GRAPHQL_ENDPOINT, {
       query: profileQuery,
       variables: { username }
@@ -77,7 +73,6 @@ app.get('/:username', async (req, res) => {
       }
     });
 
-    // Get submissions data
     const submissionsResponse = await axios.post(LEETCODE_GRAPHQL_ENDPOINT, {
       query: submissionsQuery,
       variables: { username }
@@ -90,7 +85,6 @@ app.get('/:username', async (req, res) => {
     const profileData = profileResponse.data.data.matchedUser;
     const submissionsData = submissionsResponse.data.data.recentSubmissionList;
 
-    // Calculate submission calendar
     const submissionCalendar = {};
     submissionsData.forEach(submission => {
       const timestamp = submission.timestamp;
@@ -101,7 +95,6 @@ app.get('/:username', async (req, res) => {
       }
     });
 
-    // Format response to match the existing API structure
     const response = {
       status: "success",
       message: "retrieved stats successfully",
@@ -131,6 +124,4 @@ app.get('/:username', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+export default app;
